3704f2007ff3eab5f5cc5463b9431d6b66538289
oriella27 <49071100+oriella27@users.noreply.github.com>
2026-07-19 18:27:11 -0400

    Add: python-read-input-practice - submission-0

---
+++ Python For Beginners/python-read-input-practice/submission-0.py
@@ -0,0 +1,14 @@
+def add_two_numbers() -> int:
+    integers = input().split(',')
+    integer0 = int(integers[0])
+    integer1 = int(integers[1])
+    summa = integer0 + integer1
+    return summa
+
+
+
+# do not modify below this line
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
+print(add_two_numbers())
